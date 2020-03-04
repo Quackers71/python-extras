a5c91768eac7a05c01fe8360064deaba713a7550
Quackers <robq71@gmail.com>
2020-03-04 20:14:53 +0000

    edabit challenge - Sum of People's Budget

---
+++ edabit/peepsBudget.py
@@ -0,0 +1,16 @@
+get_budgets = [
+  { "name": "John", "age": 21, "budget": 23000 },
+  { "name": "Steve",  "age": 32, "budget": 40000 },
+  { "name": "Martin",  "age": 16, "budget": 2700 }] 
+  # 65700
+
+get_budgets = [
+  { "name": "John",  "age": 21, "budget": 29000 },
+  { "name": "Steve",  "age": 32, "budget": 32000 },
+  { "name": "Martin",  "age": 16, "budget": 1600 }] 
+  # 62600
+
+print(get_budgets)
+
+#def get_budgets(lst):
+    
