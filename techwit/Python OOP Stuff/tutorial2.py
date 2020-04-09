82f0e9427c1ae823a038c08fb559a70e1fe38d3b
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-09 23:24:17 +0100

    learning classes - constructor method / methods - instance of a class

--- techwit/Python OOP Stuff/tutorial2.py
+++ techwit/Python OOP Stuff/tutorial2.py
@@ -1,17 +1,29 @@
 
-class Dog(object):
-    def __init__(self, name, age):
-        self.name = name
+class Person(object):
+    def __init__(self, name, age): # this is the constructor method - you need this when you first create your class
+        self.name = name # 'self' is the instance and name is the attribute / (parameter)
+        self.age = age
+        self.li = [1,3,4]
+
+    def speak(self): # a method
+        print("Hi, I am",self.name,'& I\'m',self.age,'years old.')
+
+    def change_age(self, age): # a method
         self.age = age
 
-    def speak(self):
-        print("Hi, I am",self.name,'& I am',self.age,'years old.')
+    def person_weight(self, weight): # another method
+        self.weight = weight
 
-    def change_age(self, age):
-        self.age = age
 
-q = Dog('Rob', 49)
-fred = Dog('Fred', 70)
-fred.change_age(65)
+q = Person('Rob', 49) # q is an object or instance of the class Person
+fred = Person('Fred', 70) # 'Fred',name & 70,age are parameters passed into the instance 'fred' of the class 'Person'
+q.change_age(25)
+q.person_weight(83)
 q.speak()
 fred.speak()
+
+print(q.name,"is",q.age)
+print(q.name,"weighs in at",q.weight,"kgs")
+print("fred.li =",fred.li)
+
+print("You won't be able to use fred.weight as you haven't yet called add_weight on the instance of 'fred'")
