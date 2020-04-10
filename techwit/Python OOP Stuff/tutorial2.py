cb49107c1fa8a582512cf8f0b5b325c7bf4e5b40
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-10 17:18:46 +0100

    added kgs to stones method in

--- techwit/Python OOP Stuff/tutorial2.py
+++ techwit/Python OOP Stuff/tutorial2.py
@@ -11,19 +11,24 @@ class Person(object):
     def change_age(self, age): # a method
         self.age = age
 
-    def person_weight(self, weight): # another method
-        self.weight = weight
+    def person_weight(self, kgs): # another method
+        self.kgs = kgs
+
+    def convert_kgsToStones(self, stones): # added my method in ;-)
+        self.stones = stones / 6.35
 
 
 q = Person('Rob', 49) # q is an object or instance of the class Person
 fred = Person('Fred', 70) # 'Fred',name & 70,age are parameters passed into the instance 'fred' of the class 'Person'
 q.change_age(25)
 q.person_weight(83)
+q.convert_kgsToStones(83)
 q.speak()
 fred.speak()
 
+
 print(q.name,"is",q.age)
-print(q.name,"weighs in at",q.weight,"kgs")
+print(q.name,"weighs in at",q.kgs,"kgs, which equates to",round(q.stones,2),"in stones")
 print("fred.li =",fred.li)
 
 print("You won't be able to use fred.weight as you haven't yet called add_weight on the instance of 'fred'")
