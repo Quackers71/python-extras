482b8ba3578b05e9db25b06ce0377a32d8c1614f
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-10 17:30:54 +0100

    added notes to the end of OOP#2

--- techwit/Python OOP Stuff/tutorial2.py
+++ techwit/Python OOP Stuff/tutorial2.py
@@ -14,15 +14,15 @@ class Person(object):
     def person_weight(self, kgs): # another method
         self.kgs = kgs
 
-    def convert_kgsToStones(self, stones): # added my method in ;-)
+    def convert_to_stones(self, stones): # added my method in ;-)
         self.stones = stones / 6.35
 
 
-q = Person('Rob', 49) # q is an object or instance of the class Person
+q = Person('Rob', 49) # q is an object or an instance of the class Person
 fred = Person('Fred', 70) # 'Fred',name & 70,age are parameters passed into the instance 'fred' of the class 'Person'
 q.change_age(25)
 q.person_weight(83)
-q.convert_kgsToStones(83)
+q.convert_to_stones(83)
 q.speak()
 fred.speak()
 
@@ -32,3 +32,7 @@ print(q.name,"weighs in at",q.kgs,"kgs, which equates to",round(q.stones,2),"in 
 print("fred.li =",fred.li)
 
 print("You won't be able to use fred.weight as you haven't yet called add_weight on the instance of 'fred'")
+
+print("\nThe idea of classes let you create multiple instances of a class including all of it's attributes i.e.")
+print("Person() - name, age, weight...")
+print("q = Person('Rob', 25, 83)")
