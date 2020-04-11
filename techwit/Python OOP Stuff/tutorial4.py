f0d4d86a270099aea09873eb23d5c5854d422d6f
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-11 10:25:22 +0100

    Completed OOP#4 Overloading Methods

---
+++ techwit/Python OOP Stuff/tutorial4.py
@@ -0,0 +1,70 @@
+# Overloading Methods
+
+class Point():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.coords = (self.x, self.y)
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
+    def __add__(self, p):
+        return Point(self.x + p.x, self.y + p.y)
+
+    def __sub__(self, p):
+        return Point(self.x - p.x, self.y - p.y)
+
+    def __mul__(self, p):
+        return self.x * p.x + self.y * p.y
+
+    def length(self):
+        import math
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __gt__(self, p):
+        return self.length() > p.length()
+
+    def __ge__(self, p):
+        return self.length() >= p.length()
+
+    def __lt__(sef, p):
+        return self.length() < p.length()
+
+    def __le__(self, p):
+        return self.length() <= p.length()
+
+    def __eq__(self, p):
+        return self.x == p.x and self.y == p.y
+    
+    def __str__(self):
+        return "(" + str(self.x) + ',' + str(self.y) + ')'
+
+    def __len__(self):
+        a = str(self.x)
+        b = str(self.y)
+        c = a + b
+        return len(c)
+
+    
+p1 = Point(3,4)
+p2 = Point(3,2)
+p3 = Point(1,3)
+p4 = Point(0,1)
+p5 = p1 + p2
+p6 = p4 - p1
+p7 = p2*p3
+
+print("p5 = p1 + p2 ",p5)
+print("p6 = p4 - p1 ", p6)
+print("p7 = p2 * p3 ", p7, "\n")
+
+print("p1 == p2 ", p1 == p2)
+print("p1 > p2  ", p1 > p2)
+print("p4 <= p3 ", p4 <= p3)
+
+print("len(p1) is ", len(p1))
+
+# See http://www.siafoo.net/article/57 for more on builtin Overloading Methods
+# Use dir(__builtins__) to view all the builtin Methods
