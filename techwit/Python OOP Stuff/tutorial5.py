e7167d961a953cee95f2f13075f88c3e51948a7c
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-11 16:52:31 +0100

    Completed OOP#5 Class Methods, Static Methods & Class Variables

--- techwit/Python OOP Stuff/tutorial5.py
+++ techwit/Python OOP Stuff/tutorial5.py
@@ -1,20 +1,36 @@
+#  Class Methods, Static Methods & Class Variables
+
 class Dog:
-    dogs = []
+    dogs = [] # a class variable
+    xc = 5
 
     def __init__(self, name):
         self.name = name
         self.dogs.append(self)
 
-    @classmethod
+    def __str__(self):
+        return str(self.name)
+
+    @classmethod # A Decorator to denote a special type of Method - which doesn't require 'self' or another parameter
     def num_dogs(cls):
         return len(cls.dogs)
 
-    @staticmethod
+    @staticmethod # Another Decorator of type staticmethod
     def bark(n):
         """barks n times"""
         for _ in range(n):
             print("Bark!")
 
+# class Math:
+#     @staticmethod
+#     def add(x, x2)
+#         return x + x2
+
 
 dex = Dog("Dexter")
 pop = Dog("Poppy")
+print("Number of instances of Dog =",Dog.num_dogs())
+
+print(dex, "&", pop,"\n")
+
+Dog.bark(5)
