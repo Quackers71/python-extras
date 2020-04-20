4da55fd250df9f38127622ce9663b0b4247b27a1
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-04-20 13:15:28 +0100

    added random enemy ships

--- techwit/Pygame Space Invaders/main.py
+++ techwit/Pygame Space Invaders/main.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("Space Invaders")
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+PURPLE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_purple_small.png"))
 
 # Player ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
@@ -21,6 +22,7 @@ RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+PURPLE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_purple.png"))
 
 #  Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
@@ -54,16 +56,32 @@ class Player(Ship):
         self.max_health = health
 
 class Enemy(Ship):
+    COLOUR_MAP = {
+                    "red": (RED_SPACE_SHIP, RED_LASER),
+                    "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+                    "blue": (BLUE_SPACE_SHIP, BLUE_LASER),
+                    "purple": (PURPLE_SPACE_SHIP, PURPLE_LASER)
+    }
+
     def __init__(self, x, y, colour, health=100):
         super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOUR_MAP[colour]
+        self.mask = pygame.mask.from_surface(self.ship_img)
 
-
+    def move(self, vel):
+        self.y += vel
+    
+    
 def main():
     run = True
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     player_vel = 5
 
@@ -80,13 +98,22 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(["red", "blue", "green", "purple"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -102,6 +129,10 @@ def main():
         if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT: # down
             player.y += player_vel
 
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+
+        redraw_window()
 
 main()
 
