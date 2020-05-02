fe96871940b7a47c02a1687a2f0633dd01b09790
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-02 21:52:10 +0100

    configured selenium to target my ec2 nginx webserver

--- techwit/Selenium/app.py
+++ techwit/Selenium/app.py
@@ -1,2 +1,8 @@
 # Selenium Tutorial #1
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
+from selenium import webdriver
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://ec2-18-130-37-136.eu-west-2.compute.amazonaws.com/")
