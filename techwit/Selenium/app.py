30a66bfa2a463fd5d22c304f04d4b4cbba065a12
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-03 14:40:36 +0100

    returned element by.ID main

--- techwit/Selenium/app.py
+++ techwit/Selenium/app.py
@@ -2,6 +2,9 @@
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -14,6 +17,17 @@ search = driver.find_element_by_name("s")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
-time.sleep(10)
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    print(main.text)
+except:
+    driver.quit()
 
-driver.quit()
+
+
+#print(driver.page_source)
+
+# time.sleep(10)
+# driver.quit()
