84f75453971cbf32d9c4c207d40b8c548d5c699e
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-03 11:44:44 +0100

    imported common Keys and searched test in element s

--- techwit/Selenium/app.py
+++ techwit/Selenium/app.py
@@ -1,8 +1,19 @@
 # Selenium Tutorial #1
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("http://ec2-18-130-37-136.eu-west-2.compute.amazonaws.com/")
+driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+driver.quit()
