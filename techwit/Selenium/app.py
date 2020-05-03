63a40b4659c4f6e81b70260419de91554755dd27
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-03 19:12:38 +0100

    article.find_e_class 'entry-summary'

--- techwit/Selenium/app.py
+++ techwit/Selenium/app.py
@@ -21,7 +21,12 @@ try:
     main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "main"))
     )
-    print(main.text)
+    
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+    
 except:
     driver.quit()
 
