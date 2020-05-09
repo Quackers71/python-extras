c27c540665f8983c6ee3f696d2f89ea17314e92e
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-09 10:37:25 +0100

    Ran 2 test cases

--- techwit/Selenium/testcase/main.py
+++ techwit/Selenium/testcase/main.py
@@ -6,11 +6,16 @@ import page
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
+        print("setup")
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.get("http://www.python.org")
 
     def test_example(self):
         print("Test")
+        assert False
+
+    def test_example_2(self):
+        print("Test2")
         assert True
 
     def not_a_test(self):
