01d8dd17c5e50552fff13925a68dabbe8d6576b1
pi <robq71@gmail.com>
2020-05-28 12:23:20 +0000

    using elif statement

--- linuxhint/helloScript.sh
+++ linuxhint/helloScript.sh
@@ -2,9 +2,12 @@
 
 count=10
 
-if [ $count -eq 8 ]
+if (( $count < 9 ))
 then 
-        echo "the condition is true"
+        echo "the first condition is true"
+elif (( $count > 9 ))
+then 
+        echo "the second condition is true"
 else
         echo "the confition is false"
 fi
