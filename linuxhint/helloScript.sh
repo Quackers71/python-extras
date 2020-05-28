3db8fa1fe35d0dbfe96c394bc64aebe84b6bfdf1
pi <robq71@gmail.com>
2020-05-28 08:31:34 +0000

    hereDoc delimeter

--- linuxhint/helloScript.sh
+++ linuxhint/helloScript.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-echo "hello bash"
-
-echo "Type something: "
-# now output using cat
-cat > output.txt
-
-echo "Type some more: "
-cat >> output.txt
-
-: '
-now output using cat
-now output using cat
-now output using cat'
-
-echo "we're all done"
+cat << kreativ
+some creative text
+and another line
+kreativ
