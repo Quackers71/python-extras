92ce3ae6e9a8a57132f7cdf643a2c9662c3b5587
pi <robq71@gmail.com>
2020-05-31 21:23:46 +0000

    file append text script

--- linuxhint/file-script2.sh
+++ linuxhint/file-script2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-read -p "Enter the file name to create : " fileName
+read -p "Enter the file name which you want to append : " fileName
 
 if [[ -f "$fileName" ]]
 then
         echo "The File \"$fileName\" exists"
+        read -p "Enter the text you want to append : " fileText
+        echo "$fileText" >> $fileName 
         exit 0
 else
         echo "The File \"$fileName\" doesn't exist"
