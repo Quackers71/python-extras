ad9c2853abb8084ee31baba3e2ad3467586d3a94
pi <robq71@gmail.com>
2020-06-02 20:07:56 +0000

    updated proMenus Script

--- linuxhint/proMenus-script.sh
+++ linuxhint/proMenus-script.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-select car in BMW MERCEDES TESLA VW AUDI
+select car in BMW MERCEDES TESLA VW AUDI EXIT
 do
-        echo "You have selected $car"
+        case $car in
+        BMW)
+                echo "BMW selected";;
+        MERCEDES)
+                echo "MERCEDES selected";;
+        TESLA)
+                echo "TESLA selected";;
+        VW)
+                echo "VW selected";;
+        AUDI)
+                echo "AUDI selected";; 
+        *)
+                echo "Error! Please select between 1..5";;
+        esac
 done
