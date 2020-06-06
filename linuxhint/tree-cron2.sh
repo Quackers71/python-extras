9b8b33dd0aecf5feef5461f2adf3fc9bbe7dce5b
pi <robq71@gmail.com>
2020-06-06 23:11:50 +0100

    working with inotify & worked on cron-logrotate ;-)

--- linuxhint/tree-cron2.sh
+++ linuxhint/tree-cron2.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+fileName="tree.txt"
+
 cd ~/
-echo `df -h /`
-echo `date`
-echo `ls -lah ~/tree.txt`
+if [[ -f "$fileName" ]]
+then
+        echo `tree` >> $fileName
+        echo `df -h /` >> $fileName
+        echo `date` >> $fileName
+        echo `ls -lah ~/$fileName` >> $fileName 
+else
+        touch $fileName
+        cp ~/tree-template.txt ~/$fileName
+fi
