6396e2e6d7c113a4d80b9179d2441c21e001fc2e
Quackers <robq71@gmail.com>
2021-08-25 20:38:33 +0100

    String slicing

--- JBarCode/Projects/temp_project/temp.py
+++ JBarCode/Projects/temp_project/temp.py
@@ -1,10 +1,22 @@
+#String slicing
 name = 'The Pebble aka Quackers'
-names = name.split(' ')
-print(name)
-print(names)
+fullstr = name[:]
+print(fullstr)
 
-new_name = ' '.join(names)
-print(new_name)
+rev_ent_fullstr = name[::-1]
+print(rev_ent_fullstr)
 
-alt_new_name = '|'.join(names)
-print(alt_new_name)
+x = name[15:23]
+print(x)
+
+x_rev = name[-8:]
+print(x_rev)
+
+x_rev_then_rev = name[-8:][::-1]
+print(x_rev_then_rev)
+
+y = name[0]
+print('The 1st character is ' + y)
+
+z = name[-8]
+print(z + ' is the best!')
