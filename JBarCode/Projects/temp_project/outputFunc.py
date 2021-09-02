32e296a56557d72561da795bf008f2630a72be17
Quackers <robq71@gmail.com>
2021-09-02 21:26:28 +0000

    basic Functions

---
+++ JBarCode/Projects/temp_project/outputFunc.py
@@ -0,0 +1,17 @@
+def temp_func():
+    print('1. Hello Function')
+    print('2. Still in the Function')
+print('Not in the Function no matter how close the line is!')
+
+print('You are here')
+temp_func()
+print('There you are!')
+
+
+temp_func()
+temp_func()
+temp_func()
+temp_func()
+
+# name = input('What\'s your name? : ')
+# print(f'Hey there {name}. What\'s your sign')
